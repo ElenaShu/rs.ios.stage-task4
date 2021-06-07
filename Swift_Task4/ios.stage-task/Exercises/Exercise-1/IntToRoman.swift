634f05de24d21b0966429a90f99f23915e3b10de
Elena <elen.shurigina@yandex.ru>
2021-06-07 18:45:19 +0300

    feat: add decision

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,79 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        var str = ""
+        
+        if self < 1 || self > 3999 { return nil }
+        var number = self
+        if number / 1000 > 0 {
+            for _ in 0..<self/1000 {
+                str.append("M")
+            }
+            number %= 1000
+        }
+        
+        if number / 100 > 0 {
+            let digit = number / 100
+            if digit > 0 && digit < 4 {
+                for _ in 0..<digit {
+                    str.append("C")
+                }
+            } else if digit == 4 {
+                str.append("CD")
+            } else if digit == 5 {
+                str.append("D")
+            } else if digit > 5 && digit < 9 {
+                str.append("D")
+                for _ in 0..<(digit-5) {
+                    str.append("C")
+                }
+            } else if digit == 9 {
+                str.append("CM")
+            }
+            number %= 100
+        }
+            
+        if number / 10 > 0 {
+            let digit = number / 10
+            if digit > 0 && digit < 4 {
+                for _ in 0..<digit {
+                    str.append("X")
+                }
+            } else if digit == 4 {
+                str.append("XL")
+            } else if digit == 5 {
+                str.append("L")
+            } else if digit > 5 && digit < 9 {
+                str.append("L")
+                for _ in 0..<(digit-5) {
+                    str.append("X")
+                }
+            } else if digit == 9 {
+                str.append("XC")
+            }
+            number %= 10
+        }
+        
+        if number > 0 {
+            if number > 0 && number < 4 {
+                for _ in 0..<number {
+                    str.append("I")
+                }
+            } else if number == 4 {
+                str.append("IV")
+            } else if number == 5 {
+                str.append("V")
+            } else if number > 5 && number < 9 {
+                str.append("V")
+                for _ in 0..<(number-5) {
+                    str.append("I")
+                }
+            } else if number == 9 {
+                str.append("IX")
+            }
+            number %= 10
+        }
+        
+        return str
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,42 @@ import Foundation
 
 final class FillWithColor {
     
+    var arr = [[Int]]()
+    
+    func colorPixel (row: Int, column: Int, oldColor: Int, newColor: Int) {
+        arr[row][column] = newColor
+        if row != 0 {
+            if arr[row-1][column] == oldColor {
+                colorPixel(row: row-1, column: column, oldColor: oldColor, newColor: newColor)
+            }
+        }
+        if column != 0 {
+            if arr[row][column-1] == oldColor {
+                colorPixel(row: row, column: column-1, oldColor: oldColor, newColor: newColor)
+            }
+        }
+        if (row+1) != arr.count {
+            if arr[row+1][column] == oldColor {
+                colorPixel(row: row+1, column: column, oldColor: oldColor, newColor: newColor)
+            }
+        }
+        if (column+1) != arr[0].count {
+            if arr[row][column+1] == oldColor {
+                colorPixel(row: row, column: column+1, oldColor: oldColor, newColor: newColor)
+            }
+        }
+        return
+    }
+    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        arr = image
+        guard image.count > 0 else { return image }
+        guard image[0].count > 0 else { return image }
+        guard row >= 0 && column >= 0 && image.count <= 50 && image[0].count <= 50 && newColor >= 0 && newColor <= 65536 else { return image }
+        guard row < image.count else { return image }
+        guard column < image[row].count else { return image }
+        guard image[row][column] >= 0 && image[row][column] <= 65536 else { return image }
+        colorPixel(row: row, column: column, oldColor: arr[row][column], newColor: newColor)
+        return arr
     }
 }
